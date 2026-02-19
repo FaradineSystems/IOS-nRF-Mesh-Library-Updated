9fbce7919e88b80f7c21bda8588e51cf75f83a0b
JosephFaradine <joseph@faradinesystems.com>
2026-02-19 16:44:27 -0500

    Updated vendor model status

--- Library/Mesh Messages/Generic/VendorModelStatus.swift
+++ Library/Mesh Messages/Generic/VendorModelStatus.swift
@@ -2,15 +2,20 @@
 import Foundation
 
 public struct VendorModelStatus: StaticMeshResponse {
-    public static let opCode: UInt32 = 0xC35900 // TODO
-
+    // public static let opCode: UInt32 = 0xC35900 // TODO
+    public var opCode: UInt32 {
+        return self.myOpCode
+    }
     public var parameters: Data? {
         return self.myParameters
     }
 
     public let myParameters: Data
+    public let myOpCode: UInt32
     
-    public init(parameters: Data) {
+    public init(parameters: Data, opCode: UInt32, modelId: Int, companyIdentifier: Int) {
         self.myParameters = parameters
+        self.myOpCode = opCode;
+        // print("Opcode is \(self.myOpCode)")
     }
 }
