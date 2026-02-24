befad1f815180bd0339f498c81221c6901f6ac2e
JosephFaradine <joseph@faradinesystems.com>
2026-02-24 15:37:49 -0500

    opcode from opcode and company id

--- Library/Mesh Messages/Generic/VendorModelMessage.swift
+++ Library/Mesh Messages/Generic/VendorModelMessage.swift
@@ -11,12 +11,13 @@ public struct VendorModelMessage: AcknowledgedMeshMessage, TransactionMessage {
     
     private let myParameters: Data
     public let modelId: Int
-    public let companyIdentifier: Int
+    public let companyIdentifier: UInt32
     
     // Custom init
-    public init(withOpCode opCode: UInt32, modelId: Int, companyIdentifier: Int, myParameters: Data) {
+    public init(withOpCode opCode: UInt32, modelId: Int, companyIdentifier: UInt32, myParameters: Data) {
         self.myParameters = myParameters
-        self.opCode = opCode
+        // self.opCode = opCode
+        self.opCode = ((0xC0 | opCode) << 16) | (companyIdentifier << 8); // Company ID is LITTLE ENDIAN hence why Nordics 0x0059 only has one byte used
         self.modelId = modelId
         self.companyIdentifier = companyIdentifier
         print("Opcode: \(opCode), model: \(modelId), company: \(companyIdentifier)")
